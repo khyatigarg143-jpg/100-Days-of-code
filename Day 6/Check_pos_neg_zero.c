3cdb867110f842645f431a48a4e97707219f8311
khyatigarg143-jpg <khyatigarg143@gmail.com>
2026-09-04 16:02:45 +0530

    Add files via upload

---
+++ Day 6/Check_pos_neg_zero.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    if(num>=0)
+    {
+        if(num==0)
+        printf("%d is zero",num);
+        else
+        printf("%d is positive",num);
+    }
+    else
+    {
+        printf("%d is negative",num);
+    }
+    return 0;
+
+}
