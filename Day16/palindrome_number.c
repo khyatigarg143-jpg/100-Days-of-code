4d8386336fd00ffc709ed87c9e9fdb91acf304a6
khyatigarg143-jpg <khyatigarg143@gmail.com>
2026-09-11 03:46:35 +0530

    Add files via upload

---
+++ Day16/palindrome_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0)
+    {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
