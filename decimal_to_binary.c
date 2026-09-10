a7acd6f80e67419a542ff965ff08a3fc0baa96c5
khyatigarg143-jpg <khyatigarg143@gmail.com>
2026-09-11 03:43:01 +0530

    Add files via upload

---
+++ decimal_to_binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, binary[32], i = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0)
+    {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    printf("Binary representation = ");
+
+    for (i = i - 1; i >= 0; i--)
+    {
+        printf("%d", binary[i]);
+    }
+
+    return 0;
+}
