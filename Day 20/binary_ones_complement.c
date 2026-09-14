93975a897c3f0aec5fea295c4082474ac2f2894a
khyatigarg143-jpg <khyatigarg143@gmail.com>
2026-09-14 08:59:14 +0530

    Add files via upload

---
+++ Day 20/binary_ones_complement.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, digit, complement = 0, place = 1;
+
+    printf("Enter a binary number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        digit = num % 10;
+
+        if (digit == 0)
+            digit = 1;
+        else
+            digit = 0;
+
+        complement = complement + digit * place;
+        place = place * 10;
+
+        num = num / 10;
+    }
+
+    printf("1's complement = %d", complement);
+
+    return 0;
+}
