a8db63063ecd2cb4d9fa7576851b22f98e4c8dd9
khyatigarg143-jpg <khyatigarg143@gmail.com>
2026-09-18 21:05:48 +0530

    Add files via upload

---
+++ Day27/centered_star_pattern.c
@@ -0,0 +1,58 @@
+/*
+Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, spaces, stars;
+
+    // Increasing part
+    for (i = 1; i <= 4; i++)
+    {
+        spaces = 4 - i;
+        stars = 2 * i - 1;
+
+        for (j = 1; j <= spaces; j++)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Decreasing part
+    for (i = 3; i >= 1; i--)
+    {
+        spaces = 4 - i;
+        stars = 2 * i - 1;
+
+        for (j = 1; j <= spaces; j++)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
